173211ed71a3e42c741e23737c192ba12ae31226
will-mei <7rainbowfalls@sina.cn>
2019-01-11 03:25:33 -0500

    rewrite script with awk, and 24 times faster

--- bin/catcsv.sh
+++ bin/catcsv.sh
@@ -1,43 +1,75 @@
 #!/bin/bash
-# output a human readable csv sheet.
-if [[ -z $1 ]] ;then
-    echo -e "usage: \v $0 <file1.csv file2.csv ... >\nmost UTF-8 encoded csv files are supported."
-fi
+# double width characters will cause some output format problem
 
-function show_table(){
-#calculate columns
-col=$(awk -F',' '{print NF}' $csv_file |sort -nru |head -1)
-
-function _p_width(){
-[[ $# -gt 0 ]] && exec 0<$1
-
-while read line
-do
-    local zh_part=${line//[a-zA-Z0-9[:punct:]]/}
-    echo $(( ${#line} + ${#zh_part} )) ;
-done<&0;
-# close when no data
-exec 0<&-
+function _format_indent(){
+    csv_file=$1
+#(11 8 13 13 14 7 11 13 13 13 4 3)
+WIDTH=($(awk '
+BEGIN{
+    FS=","
+    zh="[^\x00-\xff]+"
 }
+function _zh_check(line){
+    if(match(line, zh))
+        zh_stat=0
+    else
+        zh_stat=1
+}
+function _zh_count(field){
+    count1=0
+    for(j=1;j<length(field);++j)
+    {
+        if(match(substr(field, j, 1), zh))
+            count1=count1+1
+        }
+    count2=length(field)+count1
+    if(wid[i]<count2)
+        wid[i]=count2 
+}
+function _count(field){
+    #
+}
+# initialize column width from begining.
+{
+    if(NR == 1)
+        # init array
+        for(i=1;i<=NF;i=i+1)
+            wid[i]=length($i)
+            col=NF
+    }
+# field width count loop
+{
+    _zh_check($0)
+    # chinese characters in line
+    if(_zh_stat == 0)
+    {
+        # count zh character acount
+        for(i=1;i<=NF;i=i+1)
+        {
+            _zh_count($i)
+            }
+        }
+    else
+    {
+        for(i=1;i<=NF;i=i+1)
+            if(wid[i] < length($i))
+                wid[i]=length($i)
+        }
+    if(col<NF)
+        col=NF
+    }
+# output max values of column width and NF(col) 
+END{
+    for(i=1;i<=col;i=i+1)
+        printf("%u ",wid[i])
+    print col
+    }' $csv_file ))
+#echo ${WIDTH[*]}
 
 #calculate column width
 sep='+-'
-# array to save width of each column 
-declare -a WIDTH
-
-grep -q [^a-zA-Z0-9[:punct:]] $csv_file && has_doubl_width=1
-
-for i in $(seq 1 $col);do
-    # sort width max in column
-    #WIDTH[$i]=$(awk -F','  "{l=length(\$$i);if(l<1)l=1;print l}" $csv_file|sort -nr |head -1)
-    # chinese double width charactors
-    #if grep -q [^a-zA-Z0-9[:punct:]] $csv_file ;then
-    if [[ $has_doubl_width -eq 1 ]] ;then
-        WIDTH[$i]=$(awk -F','  "{print\$$i}" $csv_file | _p_width |sort -nr |head -1)
-    else
-        #field with no chinese
-        WIDTH[$i]=$(awk -F','  "{l=length(\$$i);if(l<1)l=1;print l}" $csv_file|sort -nr |head -1)
-    fi
+# last one of the array is NF of the line, the offset will be -2 .
+for i in $(seq 0 $((${#WIDTH[*]} -2))) ;do
 #calculate width of column
     tsep=""
     while [[ ${#tsep} -lt ${WIDTH[$i]} ]] ;do
@@ -49,55 +81,47 @@ done
 sep=${sep:0:-1}
 #echo $sep
 
-# use a delimeter inplace of space, '-' inplace of empty field 
-# print format table
-echo -e "\e[33m$csv_file \e[0m:"
-while read LINE ;do
-    # deal space sequncial ','
-    LINE=$(echo "$LINE" |sed -e "s/\ /TMPDELIMITEROFSPACE/g" \
-                           -e "s/^\,/_\,/g" \
-                           -e "s/\,\,/\,_\,/g" \
-                           -e "s/\,\,/\,_\,/g")
-    # calculate missing (field)columns number 
-    f=$(echo ${LINE//,/ } |wc -w)
-    # complete the short ones 
-    while [[ $f -lt $col ]] ;do
-        f=$[f + 1]
-        LINE+=",_"
-    done
-    # start with a sep line 
-    echo "$sep"
-    #line start
-    echo -n "| "
-    f_nu=0
-    # field completion
-    for field in ${LINE//,/ };do
-        # sep
-        field="${field//TMPDELIMITEROFSPACE/ }"
-        local space="${field//[^ ]/}"
-        local l_space=${#space}
-        # blank column 
-        [[ "$field" == '_' ]] && field=' ' && fn="-1"
-    #    echo -n "${field}"
-        f_nu=$[f_nu + 1]
-        zhpart=${field//[a-zA-Z0-9[:punct:]]/}
-    #    wid_f=$(( ${#field} + ${#zhpart} )) 
-        #wid_f=${#field}
-        #printf "%-${WIDTH[$f_nu]}s" "${field}" 
-        printf "%-$(( ${WIDTH[$f_nu]} + ${#zhpart} - ${l_space} $fn))b" "${field}" 
-        fn=''
-    #    while [[ $wid_f -lt ${WIDTH[$f_nu]} ]] ;do
-    #        echo -en " "
-    #        wid_f=$[wid_f + 1]
-    #    done
-        echo -n ' | '
-    done
-    echo ''
-done < $csv_file
-echo "$sep"
+awk -v "w=${WIDTH[*]}" -v "sep=$sep" "
+BEGIN{
+    FS=\",\"
+    split(w,wid,\" \")
+    print sep
+    }
+function _p_indent(i){
+    #printf(\"%u/%s | \",i,wid[i])
+    count1=0
+    if( match(\$i, \"[^\x00-\xff]+\") )
+        {
+            for(j=1; j<=length(\$i); ++j)
+            {
+                if(match(substr(\$i, j, 1), \"[^\x00-\xff]\"))
+                count1=count1+1
+                #next
+                }
+        f=\"%-\"wid[i]-count1\"s | \"
+        #printf(\"%u\",count1)
+        }
+    else
+        f=\"%-\"wid[i]\"s | \"
+    printf(f,\$i)
+    }
+{
+    printf(\"%s\",\"| \")
+}
+{
+    #for(i=1;i<=NF;i=i+1)
+    for(i=1;i<=${WIDTH[-1]};i=i+1)
+    _p_indent(i)
+    if(i>NF)
+        print\"\"
+    }
+{
+    print sep
+    }"  $csv_file 
+# end of format indent 
 }
 
 #
 for csv_file in $@ ;do
-    [[ -f $csv_file ]] && show_table || echo -e "\e[33m$csv_file \e[0m:\n csv file \"$csv_file\" not found!"
+    [[ -f $csv_file ]] && _format_indent $csv_file || echo -e "\e[33m$csv_file \e[0m:\n csv file \"$csv_file\" not found!"
 done
