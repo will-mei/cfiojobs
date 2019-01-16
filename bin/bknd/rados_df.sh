10d0a75fa9add432978e00fe7031c767bf074f95
will-mei <7rainbowfalls@sina.cn>
2019-01-16 07:08:59 -0500

    update script output

--- bin/bknd/rados_df.sh
+++ bin/bknd/rados_df.sh
@@ -9,7 +9,13 @@ job_runtime=$4
 rados_log_name=${job_name}_rados-df
 output_file=$output_dir/rados_df.csv
 
+echo "
+mon: $ceph_mon
+pool: $pool_name
+parametters: $@"
+
 function _job_batch_check(){
+    echo "$$ wait round end"
     #check job_batch_index 
     recover_log=$output_dir/recover.log
     job_batch_info=$(sed -n 1p $recover_log)
@@ -24,19 +30,25 @@ function _job_batch_check(){
 
     ###########################
     ssh -n -q $ceph_mon 'rados df --format=json-pretty ' >$output_dir/$rados_log_name.before.json
+    echo "$$ before.json $(date)"
 
     # wait job end
     _job_batch_check
 
     ssh -n -q $ceph_mon 'rados df --format=json-pretty ' >$output_dir/$rados_log_name.after.json
+    echo "$$ after.json $(date)"
 
     ###########################
     bw_iops="$(python $(dirname $0)/rados_df.py $pool_name $job_name $output_dir/$rados_log_name.before.json $output_dir/$rados_log_name.after.json)"
+    echo "$$ get bw_iops $bw_iops"
 
     ###########################
     echo "${job_name/-/},${bw_iops// /}" >> $output_file
+    echo "$$ result: ${job_name/-/},${bw_iops// /}"
 
 # rm -rf $output_dir/${rados_log_name}*.json
+# echo "$$ clean json $(date)"
 
 # start update csv script in background 
 bash $(dirname $0)/rados_df_update_avg.sh $output_dir &>/dev/null & 
+echo "$$ start update script $(date)"
--- bin/bknd/rados_df_update_avg.sh
+++ bin/bknd/rados_df_update_avg.sh
@@ -5,31 +5,41 @@ lock_file=$0.running
 trap "rm -rf $lock_file" 1 2 15
 
 # waiting end of test
+echo "wait test $(date)"
 while  ps aux |grep $(whoami) |grep cfiojobs |grep '\-\-'fio'\ ' |grep -v grep |grep -q $output_dir ;do
     sleep 5
 done
+echo "test end $(date)"
 
 # join inut and output files names
 output_dir=$1
 source_csv=$output_dir/rados_df.csv
 target_csv_list=$(ls $output_dir/_report/*_avg.csv)
 
+echo "
+output_dir: $output_dir
+source_csv: $source_csv
+target_list: $target_csv_list"
+
 # update function 
 function _update_csv(){
     # update title
-    sed -i "1 s/$/\,ceph_bw(MiB/s)\,ceph_iops/g" $avg_report_csv
+    sed -i "1 s/$/\,ceph_bw\(MiB\/s\)\,ceph_iops/g" $avg_report_csv
     while read LINE ;do
         bp=${LINE%%,*}
         content=${LINE#*,}
         # attatch lines
         sed -i "/${bp}/s/$/&\,${content}/g" $avg_report_csv 
     done < $source_csv 
+    echo "update $source_csv to $avg_report_csv done"
 }
 
 # update all avg worksheet csv 
 for avg_report_csv in $target_csv_list ;do
     if [[ -f $source_csv ]] && [[ -f $avg_report_csv ]] ;then 
-        _update_ceph_df2csv 
+        _update_csv 
+    else
+        echo "$source_csv or $avg_report_csv file not found"
     fi
 done
 
